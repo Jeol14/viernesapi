5c5008d90580f12a5d69c81dc47f5747a48763eb
Jeol14 <jolmos10@cuc.edu.co>
2023-10-06 15:13:43 -0500

    commit 5

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import usuariosRoutes from './routes/usuarios.routes.js'
+import productosRoutes from './routes/productos.routes.js'
+import ventasRoutes from './routes/ventas.routes.js'
+
 import indexRoutes from './routes/index.routes.js'
 const app=express()
 //const port=3005
 app.use(express.json())
 app.use('/api',usuariosRoutes)
+app.use('/api',productosRoutes)
+app.use('/api',ventasRoutes)
 app.use(indexRoutes)
 app.use((req, res, next)=>{
     res.status(404).json({
